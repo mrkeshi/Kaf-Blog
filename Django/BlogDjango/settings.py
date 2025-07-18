02bbd4196e58a42db3e61b02f517592ed94456c5
Alireza Keshavrz <66950773+mrkeshi@users.noreply.github.com>
2025-07-19 00:28:39 +0330

    add corsheaders
    add corsheaders

--- Django/BlogDjango/settings.py
+++ Django/BlogDjango/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "material",
 
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
